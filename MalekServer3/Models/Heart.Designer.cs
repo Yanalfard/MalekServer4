ac8b32eb22dc71f953864ceb869a36fbb739f6a8
Mehdi Sahandi <mehdisahandi@gmail.com>
2022-08-06 20:24:28 +0430

    update

--- MalekServer3/Models/Heart.Designer.cs
+++ MalekServer3/Models/Heart.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Mehdi\source\Gitlab\MalekServer4\MalekServer3\Models\Heart.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mehdi\Source\Repos\MalekServer4\MalekServer3\Models\Heart.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
